b884c56c4c6c5fd10fc51cff783dc62295004f14
Corey Donohoe <atmos@atmos.org>
2017-03-30 22:12:50 -0700

    add specs for pipeline promotion

--- spec/lib/escobar/heroku/pipeline_promotion_request_spec.rb
+++ spec/lib/escobar/heroku/pipeline_promotion_request_spec.rb
@@ -18,8 +18,49 @@ describe Escobar::Heroku::PipelinePromotionRequest do
     stub_kolkrabbi_response("#{pipeline_path}/repository")
   end
 
-  it "has a unique cache key" do
-    pending "oh well"
+  it "can promote a pipeline" do
+    stub_heroku_response("/apps/760bc95e-8780-4c76-a688-3a4af92a3eee")
+
+    response = fixture_data("api.github.com/repos/atmos/slash-heroku/index")
+    stub_request(:get, "https://api.github.com/repos/atmos/slash-heroku")
+      .with(headers: default_github_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.github.com/repos/atmos/slash-heroku/branches/master")
+    stub_request(:get, "https://api.github.com/repos/atmos/slash-heroku/branches/master")
+      .with(headers: default_github_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.github.com/repos/atmos/slash-heroku/deployments/22062424")
+    stub_request(:post, "https://api.github.com/repos/atmos/slash-heroku/deployments")
+      .with(headers: default_github_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.heroku.com/pipeline-promotions/5cf65076-74be-4335-9387-26ef2c08678f")
+    stub_request(:post, "https://api.heroku.com/pipeline-promotions")
+      .with(headers: default_heroku_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.heroku.com/pipeline-promotions/5cf65076-74be-4335-9387-26ef2c08678f/promotion-targets")
+    stub_request(:get, "https://api.heroku.com/pipeline-promotions/5cf65076-74be-4335-9387-26ef2c08678f/promotion-targets")
+      .with(headers: default_heroku_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/releases/23fe935d-88c8-4fd0-b035-10d44f3d9059")
+    stub_request(:get, "https://api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/releases/23fe935d-88c8-4fd0-b035-10d44f3d9059")
+      .with(headers: default_heroku_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/slugs/c782eba3-db0f-44cd-a8cb-a7a3d41ef831")
+    stub_request(:get, "https://api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/slugs/c782eba3-db0f-44cd-a8cb-a7a3d41ef831")
+      .with(headers: default_heroku_headers)
+      .to_return(status: 200, body: response, headers: {})
+
+    response = fixture_data("api.github.com/repos/atmos/slash-heroku/deployments/22062424/statuses/pending-1")
+    stub_request(:post, "https://api.github.com/repos/atmos/slash-heroku/deployments/22062424/statuses")
+      .with(headers: default_github_headers)
+      .to_return(status: 200, body: response, headers: {})
+
     releases = pipeline.promote(app, targets, "production", false, {})
     expect(releases).to_not be_empty
   end
