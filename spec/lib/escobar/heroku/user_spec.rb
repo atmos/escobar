3fbcc4ecc437b7a145a77e63f05291cccae0b498
Yannick Schutz <me@yannick.io>
2017-06-06 17:00:05 +0200

    :robot: :policeman:

--- spec/lib/escobar/heroku/user_spec.rb
+++ spec/lib/escobar/heroku/user_spec.rb
@@ -6,21 +6,19 @@ RSpec.describe Escobar::Heroku::User do
   let(:user) { Escobar::Heroku::User.new(client.heroku) }
 
   it "can view the app" do
+    can_view = fixture_data("api.heroku.com/capabilities/can_view")
     stub_request(:put, "https://api.heroku.com/users/~/capabilities")
       .with(headers: default_heroku_headers("3.capabilities"))
-      .to_return(
-        status: 200, body: fixture_data("api.heroku.com/capabilities/can_view")
-      )
+      .to_return(status: 200, body: can_view)
 
     expect(user.can_view_app?(id)).to be_truthy
   end
 
   it "cannot view the app" do
+    cannot_view = fixture_data("api.heroku.com/capabilities/cannot_view")
     stub_request(:put, "https://api.heroku.com/users/~/capabilities")
       .with(headers: default_heroku_headers("3.capabilities"))
-      .to_return(
-        status: 200, body: fixture_data("api.heroku.com/capabilities/cannot_view")
-      )
+      .to_return(status: 200, body: cannot_view)
 
     expect(user.can_view_app?(id)).to be_falsey
   end
